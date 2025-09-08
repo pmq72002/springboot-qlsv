c97193dec23dfd684105792fc57d574e67852ae3
pmq72002 <pmqchoke@gmail.com>
2025-09-08 11:03:25 +0700

    add constant folder

---
+++ src/main/java/com/pmq/spring/qlsv/constant/SinhVienSub.java
@@ -0,0 +1,15 @@
+package com.pmq.spring.qlsv.constant;
+
+public class SinhVienSub {
+    private  SinhVienSub(){}
+    public  static final String MSV = "msv";
+    public  static final String NAMESV = "tensv";
+    public  static final String NAMESUB = "tenMH";
+    public  static final String NUMSUB = "soTiet";
+    public  static final String SCOREQT = "diemQT";
+    public  static final String SCORETP = "diemTP";
+    public  static final String SCORESUM = "diemTongKet";
+    public  static final String PASS = "quaMon";
+
+
+}
